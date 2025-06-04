ede9100d05e3594d36e581f94fdcddffa43e12fd
Anton <shevchenko.anton.2004@gmail.com>
2025-06-04 02:07:04 +0200

    Update sign_verify.py

--- sign_verify.py
+++ sign_verify.py
@@ -12,7 +12,7 @@ def sign_file_and_save_signature(private_key_pem: bytes, file_path: str, signatu
     signature = pkcs1_15.new(key).sign(h)
     with open(signature_path, 'wb') as f:
         f.write(signature)
-    print(f"✅ Podpis zapisany do: {signature_path}")
+    print(f"Signature saved to: {signature_path}")
 
 def verify_signature_from_file(public_key_pem: bytes, file_path: str, signature_path: str) -> bool:
     key = RSA.import_key(public_key_pem)
@@ -23,16 +23,16 @@ def verify_signature_from_file(public_key_pem: bytes, file_path: str, signature_
     h = SHA256.new(file_data)
     try:
         pkcs1_15.new(key).verify(h, signature)
-        print("✅ Podpis jest prawidłowy.")
+        print("Signature is valid.")
         return True
     except (ValueError, TypeError):
-        print("❌ Podpis nieprawidłowy!")
+        print("Signature is invalid.")
         return False
 
 if __name__ == "__main__":
     priv_key, pub_key = generate_rsa_keys()
 
-    file_path = "dokument.pdf"
+    file_path = "document.pdf"
     signature_path = file_path + ".sig"
 
     sign_file_and_save_signature(priv_key, file_path, signature_path)
